85c2d9afd2ffc6e2c152f2bef1a277b5aebae871
Tasfia Roponti <144715367+TasfiaR06@users.noreply.github.com>
2024-04-17 12:07:09 -0400

    Create Tasfia.js

---
+++ Tasfia.js
@@ -0,0 +1,13 @@
+//Loader
+window.addEventListener('load', () => {
+  const loader = document. querySelector ('. loader');
+setTimeout（（）=> ｛
+loader-setAttribute('style', 'opacity: 0;');
+7, 500):
+setTimeout () → {
+loader - parentNode. removeChild(loader);
+document.body-style.overflow = 'initial';
+7, 1000):
+7):
+ent on the page.
+^
