8a2b1bb09cf77d02c32830b5f7d327faf6717d1b
Tasfia Roponti <144715367+TasfiaR06@users.noreply.github.com>
2024-04-17 12:10:49 -0400

    Update Tasfia.js

--- Tasfia.js
+++ Tasfia.js
@@ -1,13 +1,12 @@
 //Loader
 window.addEventListener('load', () => {
-  const loader = document. querySelector ('. loader');
-setTimeout（（）=> ｛
-loader-setAttribute('style', 'opacity: 0;');
-7, 500):
-setTimeout () → {
-loader - parentNode. removeChild(loader);
-document.body-style.overflow = 'initial';
-7, 1000):
-7):
-ent on the page.
-^
+  const loader = document.querySelector ('.loader');
+setTimeout (()=> ｛
+    loader-setAttribute('style', 'opacity: 0;');
+}, 500);
+setTimeout (() => {
+   loader.parentNode.removeChild(loader);
+   document.body.style.overflow = 'initial';
+   }, 1000);
+});
+
