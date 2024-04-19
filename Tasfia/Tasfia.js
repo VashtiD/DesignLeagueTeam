1dfd5410ad7872fe4f50e0a466cbbb26d03d77b1
Tasfia Roponti <144715367+TasfiaR06@users.noreply.github.com>
2024-04-19 12:51:37 -0400

    Update Tasfia.js

--- Tasfia/Tasfia.js
+++ Tasfia/Tasfia.js
@@ -10,3 +10,13 @@ setTimeout (() => {
    }, 1000);
 });
 
+const coinSfx = new Sfx('sfx/Mario Coin.wav');
+const pipeSfx = new Sfx('sfx/Mario  Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav');
+const container = document.querySelector('.content');
+let ninja;
+
+function Sfx(src) {
+  this.sound = document.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute('preload', 'auto');
